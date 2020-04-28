d619849fd43d1c407961c13cd76939017c3918b0
Quinten <quinten.twisk@hva.nl>
2020-04-28 16:09:32 +0200

    E_EEPROM works and is tested. Still could use some work though (now for real)
    Former-commit-id: 781798e46d9422907371111f6942e3a2f9a60477 [formerly 88b927568a4f17d5c572372e8f126d6ca0a43f8b] [formerly c2337b92a46d71680ca93fbb21f6dd6d1165edc4 [formerly 5c7b89bb65b78c5d942d0cfe3bc394d4252f4afe]]
    Former-commit-id: e70abd5922eab0a1f78bb795be9dbc0a43e884ce [formerly 10ed14cd064bb984956f586ecd8f8691672303e2]
    Former-commit-id: 4a38609cace9224303a9d88e68bf09c38d3ce914

---
+++ device/Arduino/powersensor/flash_helper.c
@@ -0,0 +1,13 @@
+#include "flash_helper.h"
+
+uint16_t FLASH_EraseSector(uint32_t Sector, uint8_t VoltageRange)
+{
+  FLASH_Erase_Sector(Sector, VoltageRange);
+  return HAL_FLASH_ERROR_NONE;
+}
+
+uint16_t FLASH_ProgramHalfWord(uint16_t Address, uint16_t Data)
+{
+  HAL_FLASH_Program(FLASH_TYPEPROGRAM_HALFWORD, (uint32_t) Address, (uint16_t) Data);
+  return HAL_FLASH_ERROR_NONE;
+}
---
+++ host/compile.txt
@@ -0,0 +1,1 @@
+g++ PowerSensor.cc pstest.cc -lpthread -fopenmp
