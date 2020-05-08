00da9968c15ea9bda69fc1f3342b5df0cf1d861e
Quinten <quinten.twisk@hva.nl>
2020-05-08 19:08:38 +0200

    trying an improved emulated EEPROM algorithm
    Former-commit-id: 39a1cd8cdb517880ed1b989654aa5b458e0a5047 [formerly 32da079a4d5680e38b1668d40edcb4078937c3ff] [formerly bfcc87a4a507c02691255a5ec49eda7342ff049d [formerly 87b82a6a631680cdee4949b6bd82c71942553d58]]
    Former-commit-id: f9f996e1c59a49ab9177df03bf1a259c2189a2a3 [formerly b98fe178c6a89c73e817cd9ea4429df2e0d277e6]
    Former-commit-id: b64514332dfd42c89f2c43fb91051b03a335af77

--- device/Arduino/test/test.ino
+++ device/Arduino/test/test.ino
@@ -1,20 +1,42 @@
-uint16_t challa= 0xAAFF;
-uint8_t grappie1;
-uint8_t grappie2;
-uint16_t *pointer;
+struct Sensor {
+  float volt;
+  float type;
+  float level;
+};
+
+Sensor sensor;
+
+const uint16_t SIZE_16B = sizeof(sensor)/2;
+
+uint16_t eeprom[SIZE_16B];
 
 void setup() {
   Serial.begin(9600);
-  
-  pointer = &challa;
 
-  memcpy(&grappie1, &pointer, sizeof(uint8_t));
-  memcpy(&grappie2, &pointer + sizeof(grappie1), sizeof(grappie2)); 
+  sensor.volt = 3.3;
+  sensor.type = .185;
+  sensor.level = 3.3455;
+
+  uint16_t *p_data = (uint16_t *)&sensor;
+  uint16_t *p_eeprom = (uint16_t *)eeprom;
+
+  for (int i = 0; i < SIZE_16B; i++) {
+    memcpy(p_eeprom, p_data, sizeof(uint16_t));
+    p_data++;
+    p_eeprom++;
+  } 
 }
 
 
 void loop() {
-  Serial.println(grappie1, BIN);
-  Serial.println(grappie2, BIN);
+  Serial.println("Copied: ");
+  Serial.println(sensor.volt, BIN);
+  Serial.println(sensor.type, BIN);
+  Serial.println(sensor.level, BIN);
+  Serial.println("To: ");
+  for (int i = 0; i < SIZE_16B; i++) {
+    Serial.println(eeprom[i], BIN);
+  }
+
   delay(1000);
 }
