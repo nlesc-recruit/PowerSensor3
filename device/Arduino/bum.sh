76921039a2169f0c6caf7b301fa78367a812489a
Quinten <quinten.twisk@hva.nl>
2020-05-08 19:52:12 +0200

    implemented new algorithm for write function of emulated EEPROM
    Former-commit-id: 15f3c018b3d946777903a3bf89abdc01d4231c92 [formerly 0a604b14e1d669c606e6ed3592f71c716e17e451] [formerly 062f5343f249b49bc8be1a212ca3f8b01f116e59 [formerly 3f9a921185e64408664e7b166a0775dabe0f3096]]
    Former-commit-id: 2195c56fc97a8000a9802d9ca32de6ef65d9e8a8 [formerly f13877492eb8283158fb5eb876ccef23e049fd04]
    Former-commit-id: a2e75cf8163129475a6c35d8ba04131cf80d4d10

---
+++ device/Arduino/bum.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Compiling sketch: test"
+arduino-cli compile --fqbn STM32:stm32:Disco:pnum=DISCO_F407VG,usb=CDCgen powersensor
+echo "Compiling complete"
+echo "Uploading sketch: test, to: /dev/ttyACM0"
+arduino-cli upload -p /dev/ttyACM0 --fqbn STM32:stm32:Disco:pnum=DISCO_F407VG,usb=CDCgen powersensor
+echo "Uploading complete"
+echo "Waiting 25 (s) for device reset"
+sleep 25
+echo "Opening serial port with PlatformIO"
+pio device monitor -p /dev/ttyACM1
--- device/Arduino/test.sh
+++ device/Arduino/test.sh
@@ -6,6 +6,14 @@ echo "Uploading sketch: test, to: /dev/ttyACM0"
 arduino-cli upload -p /dev/ttyACM0 --fqbn STM32:stm32:Disco:pnum=DISCO_F407VG,usb=CDCgen test
 echo "Uploading complete"
 echo "Waiting 25 (s) for device reset"
-sleep 25
+sleep 5
+echo "20 (s) remaining"
+sleep 5
+echo "15 (s) remaining"
+sleep 5
+echo "10 (s) remaining"
+sleep 5
+echo "5 (s) remaining"
+sleep 5
 echo "Opening serial port with PlatformIO"
 pio device monitor -p /dev/ttyACM1
