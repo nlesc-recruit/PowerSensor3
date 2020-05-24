d8631cfd3887a5d9d484799e68dae3adbb9fe1f7
quintentwisk <quinten.twisk@hotmail.com>
2020-05-24 15:14:18 +0200

    wrote script that accurately measures ps speed
    Former-commit-id: 57cf127cc825f9112424c530f68aee28c42b8f1c [formerly e54338e96ff647b6e81ea4c5610ded98f5808022] [formerly 2d3c3931e8c7f32c4b0ff700389e05d70622ac79 [formerly d37a9a6e885845abb00810720ee2f9d2a1130ac4]]
    Former-commit-id: 3d9129abff7d6f8943254ed1045be4f0b1a6b289 [formerly b1468667c2e7f8871947e985c8cd9df8fbf388f7]
    Former-commit-id: 96fabd2cb272e78ad0ab51d245ce7d4c6d4d99b7

---
+++ test/measurespeeds.py
@@ -0,0 +1,97 @@
+import re
+import matplotlib.pyplot as plt
+import numpy as np
+
+imported_data = marker_found = None
+marker_found = True
+sensordata = [[],[],[],[],[],[],[]]
+
+with open('output_m2.txt', 'r') as reader:
+  imported_data = reader.readlines()
+
+markers = []
+count = 0
+
+for datum in imported_data:
+    count += 1
+    if marker_found is True:
+        if re.search("S",datum):
+            obj = re.search(" ",datum)
+            rest = datum[obj.span()[1]:len(datum)]
+            for i in range(6):
+                obj = re.search(" ",rest)
+                sensordata[i].append(float(rest[0:obj.span()[0]]))
+                rest = rest[obj.span()[1]:len(rest)]
+            sensordata[6].append(float(rest))
+    if re.search("M",datum):
+        markers.append(count)
+
+print("processed imported data\n")
+
+highest_values = []
+
+# find highest value
+time = sensordata[0][10]
+count = 0
+stime = time + 1.0
+
+samples = []
+
+for datum in sensordata[0]:
+    if datum > stime:
+        samples.append(count)
+        count = 0
+        stime = datum + 1.0
+    count += 1
+
+#for i in range(7):
+ #   print(sensordata[i][10000])
+
+print(samples)
+
+exit()
+
+
+print(highest_values)
+size = len(sensordata[0]) 
+print(size)
+
+x_axis = np.arange(size)
+
+start = 10
+end = size
+
+#y_axis = sensordata[6]
+
+fig, ax = plt.subplots()
+ax.set_ylim([0,30])
+
+legendthings = ['','','12 V PSU', '12 V PSU', '12 V riser cable', '3.3 V riser cable', 'Sum']
+
+#for i in range(2,7):
+    #ax.plot(x_axis, y_axis, label=legendthings[i])
+
+x_axis = x_axis[start:end]
+#y_axis = y_axis[start:end]
+
+#for i in range (2,7):
+y_axis = sensordata[1]
+y_axis = y_axis[start:end]
+ax.plot(x_axis, y_axis, label=legendthings[i])
+
+
+#for value in highest_values:
+ #  plt.axhline(value, c='r',linestyle='--')
+ #   plt.text(end+130,value+2,str(int(value)))
+    #for marker in markers:
+#    plt.axvline(marker,0,250,c='grey',linestyle='--')
+
+plt.title('Power draw on individual sensors')
+plt.xlabel('samples', fontsize=12)
+plt.ylabel('power (w)', fontsize=12)
+
+plt.legend()
+plt.show()
+
+#plt.savefig('variables.png')
+
