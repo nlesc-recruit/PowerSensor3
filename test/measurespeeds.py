50f0edb8083cffb3d2ac184194005420bbfc49fe
quintentwisk <quinten.twisk@hotmail.com>
2020-06-02 09:20:36 +0200

    sps test output
    Former-commit-id: 69005eddac16e8a5d0ea8260ef4f11e00dab8019 [formerly 0e2149741d44377d5a3424d653ca714d08c0f53d] [formerly 9852873668d92a7fcc3654bf3ade7c379f5d0ed6 [formerly 4b1ffa2b0a8371180da003425c9512a7c1b2cb9a]]
    Former-commit-id: e1f6c613c32b4ec81018e08d40eb90f150b434df [formerly ed9f44880a27546d6c12c03040c3f395070a98bf]
    Former-commit-id: 3aef6ee97c4251aac1a9a26f679c9038bf9b4605

--- test/measurespeeds.py
+++ test/measurespeeds.py
@@ -1,104 +1,98 @@
 import re
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import numpy as np
-
-imported_data = marker_found = None
-marker_found = True
-sensordata = [[],[],[],[],[],[],[]]
-
-with open('output.txt', 'r') as reader:
-  imported_data = reader.readlines()
-
-markers = []
-count = 0
-
-for datum in imported_data:
-    count += 1
-    if marker_found is True:
-        if re.search("S",datum):
-            obj = re.search(" ",datum)
-            rest = datum[obj.span()[1]:len(datum)]
-            for i in range(6):
-                obj = re.search(" ",rest)
-                sensordata[i].append(float(rest[0:obj.span()[0]]))
-                rest = rest[obj.span()[1]:len(rest)]
-            sensordata[6].append(float(rest))
-    if re.search("M",datum):
-        markers.append(count)
-
-print("processed imported data\n")
-size = len(sensordata[0])
-print(size)
-
-highest_values = []
-
-# find highest value
-time = sensordata[0][10]
-count = 0
-stime = time + 1.0
-
-samples = []
-
-for datum in sensordata[0]:
-    if datum > stime:
-        samples.append(count)
-        count = 0
-        stime = datum + 1.0
-    count += 1
-
-#for i in range(7):
- #   print(sensordata[i][10000])
-
-print(samples)
-
-with open('input.txt', 'w+') as writer:
-        for x in samples:
-            writer.write(str(x))
-            writer.write("\n")
-
-exit()
+from mpl_toolkits.axes_grid.inset_locator import (inset_axes, InsetPosition, mark_inset)
 
 
-print(highest_values)
-size = len(sensordata[0]) 
-print(size)
+imported_data = [[],[],[],[],[]]
 
-x_axis = np.arange(size)
+with open('input1.txt', 'r') as reader:
+  imported_data[0] = reader.readlines()
 
-start = 10
-end = size
+with open('input2.txt', 'r') as reader:
+  imported_data[1] = reader.readlines()
 
-#y_axis = sensordata[6]
+with open('input3.txt', 'r') as reader:
+  imported_data[2] = reader.readlines()
+
+with open('input4.txt', 'r') as reader:
+  imported_data[3] = reader.readlines()
+
+with open('input5.txt', 'r') as reader:
+  imported_data[4] = reader.readlines()
+
+x_axis = np.arange(1,20)
+
+sampledata = [[],[],[],[],[]]
+average = []
+
+
+for i in range(5):
+    for j in range(0,19):
+        sampledata[i].append(int(imported_data[i][j]))
+
+value = 0
+
+for i in range (0,19):
+    for j in range (5):
+        value += sampledata[j][i] 
+    average.append(value/5)
+    value = 0
+
+
+print(sampledata)
 
 fig, ax = plt.subplots()
-ax.set_ylim([0,30])
+ax.set_ylim([160000,200001])
+plt.yticks(np.arange(160000,200001,step=10000))
+ax.yaxis.set_minor_locator(plt.MultipleLocator(2500))
 
-legendthings = ['','','12 V PSU', '12 V PSU', '12 V riser cable', '3.3 V riser cable', 'Sum']
+
+legendthings = ['Test #1', 'Test #2', 'Test #3', 'Test #4', 'Test #5']
+colors = ['b','g','c','m','y']
+
 
 #for i in range(2,7):
     #ax.plot(x_axis, y_axis, label=legendthings[i])
 
-x_axis = x_axis[start:end]
 #y_axis = y_axis[start:end]
 
 #for i in range (2,7):
-y_axis = sensordata[1]
-y_axis = y_axis[start:end]
+#y_axis = sensordata[]
+#y_axis = y_axis[start:end]
 #ax.plot(x_axis, y_axis, label=legendthings[i])
 
+begin = 0
+end = 10
+
+for i in range(5):
+    ax.plot(x_axis[begin:end], sampledata[i][begin:end], label=legendthings[i], linewidth='0.7', color=colors[i], marker=".")
+
+ax.plot(x_axis[begin:end], average[begin:end], label='Average', color='r', linestyle='--')
+
+#ax2 = plt.axes([0,0,1,1])
+#ip = InsetPosition(ax, [0.4,0.2,0.5,0.5])
+#ax2.set_axes_locator(ip)
+
+#for i in range(5):
+#    ax.plot(x_axis, sampledata[i], label=legendthings[i], linewidth='0.7', color=colors[i], marker=".")
+
+#ax2.plot(x_axis, average, label='Average', color='r', linestyle='--')
 
 #for value in highest_values:
  #  plt.axhline(value, c='r',linestyle='--')
  #   plt.text(end+130,value+2,str(int(value)))
     #for marker in markers:
 #    plt.axvline(marker,0,250,c='grey',linestyle='--')
+fig.subplots_adjust(left=0.2)
 
-#plt.title('Power draw on individual sensors')
-#plt.xlabel('samples', fontsize=12)
-#plt.ylabel('power (w)', fontsize=12)
+plt.title("Amount of samples per second")
+plt.xlabel('second', fontsize=12)
+plt.ylabel('samples', fontsize=12)
+plt.grid(True)
 
-#plt.legend()
+plt.legend()
 #plt.show()
 
-#plt.savefig('variables.png')
+plt.savefig('sps.png')
 
