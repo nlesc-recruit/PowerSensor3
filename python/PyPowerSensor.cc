75583fabde154aae4a226d555dbb09b67f2601f9
Leon Oostrum <l.oostrum@esciencecenter.nl>
2022-04-25 18:03:52 +0200

    Style fixes

--- python/PyPowerSensor.cc
+++ python/PyPowerSensor.cc
@@ -6,11 +6,16 @@ namespace py = pybind11;
 
 
 PYBIND11_MODULE(powersensor, m) {
-  m.def("Joules", &PowerSensor::Joules, "Compute energy usage between two states", py::arg("first_state"), py::arg("second_state"), py::arg("sensor_pair"));
-  m.def("seconds", &PowerSensor::seconds, "Compute time difference between two states", py::arg("first_state"), py::arg("second_state"));
-  m.def("Watt", &PowerSensor::Watt, "Compute average power between two states", py::arg("first_state"), py::arg("second_state"), py::arg("sensor_pair"));
-  m.def("Volt", &PowerSensor::Volt, "Compute average voltage between two states", py::arg("first_state"), py::arg("second_state"), py::arg("sensor_pair"));
-  m.def("Ampere", &PowerSensor::Ampere, "Compute average current between two states", py::arg("first_state"), py::arg("second_state"), py::arg("sensor_pair"));
+  m.def("Joules", &PowerSensor::Joules, "Compute energy usage between two states",
+    py::arg("first_state"), py::arg("second_state"), py::arg("sensor_pair"));
+  m.def("seconds", &PowerSensor::seconds, "Compute time difference between two states",
+    py::arg("first_state"), py::arg("second_state"));
+  m.def("Watt", &PowerSensor::Watt, "Compute average power between two states",
+    py::arg("first_state"), py::arg("second_state"), py::arg("sensor_pair"));
+  m.def("Volt", &PowerSensor::Volt, "Compute average voltage between two states",
+    py::arg("first_state"), py::arg("second_state"), py::arg("sensor_pair"));
+  m.def("Ampere", &PowerSensor::Ampere, "Compute average current between two states",
+    py::arg("first_state"), py::arg("second_state"), py::arg("sensor_pair"));
 
   py::class_<PowerSensor::PowerSensor>(m, "PowerSensor")
     .def(py::init<const char*>())
