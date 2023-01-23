65324cb36f8b81fc544611c82e76c10d0771610d
Leon Oostrum <l.oostrum@esciencecenter.nl>
2023-01-23 13:14:19 +0100

    Remove clearDisplay argument

--- device/BLACKPILL_F401CC/PowerSensor/display.h
+++ device/BLACKPILL_F401CC/PowerSensor/display.h
@@ -2,4 +2,4 @@ void initDisplay();
 void deinitDisplay();
 void clearDisplay();
 void displaySensor(const int sensorPairName, const float amp, const float volt,
-                   const float watt, const float totalWatt, const bool clearDisplay = false);
+                   const float watt, const float totalWatt);
