7a9ed537f4e9e7ac4864374d80d8a27dff582c2e
Leon Oostrum <l.oostrum@esciencecenter.nl>
2023-01-23 13:12:07 +0100

    Use DMA-supporting classes for display; clear by fillScreen as that is fast now

--- device/BLACKPILL_F401CC/PowerSensor/display.h
+++ device/BLACKPILL_F401CC/PowerSensor/display.h
@@ -1,4 +1,5 @@
 void initDisplay();
 void deinitDisplay();
+void clearDisplay();
 void displaySensor(const int sensorPairName, const float amp, const float volt,
                    const float watt, const float totalWatt, const bool clearDisplay = false);
