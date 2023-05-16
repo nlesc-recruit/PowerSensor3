c8e081c25c714d29029f5c6f836dc5e05d9967dc
Leon Oostrum <l.oostrum@esciencecenter.nl>
2023-05-16 14:11:34 +0200

    Add python script to find stm32duino directory (platform-independent)

---
+++ python/get_arduino_stm32_directory.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from pathlib import Path
+from shutil import which
+import subprocess
+
+import yaml
+
+
+if __name__ == '__main__':
+
+    arduino_cli = which('arduino-cli')
+    if arduino_cli is None:
+        raise ValueError("arduino-cli not found")
+
+    raw_config = subprocess.Popen([arduino_cli, 'config', 'dump'], stdout=subprocess.PIPE).communicate()[0]
+    config = yaml.safe_load(raw_config)
+    data_dir = Path(config['directories']['data'])
+
+    stm32_dir = data_dir / 'packages' / 'STMicroelectronics' / 'hardware' / 'stm32'
+    if not stm32_dir.is_dir():
+        raise ValueError('stm32duino directory not found, is it installed?')
+    # the subfolders of stm32 have the stm32duino version as name
+    # pick the latest version
+    stm32_dir = list(stm32_dir.glob('*'))[-1]
+    print(stm32_dir)
