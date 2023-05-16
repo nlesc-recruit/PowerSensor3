61cdd43a234b06e543521e9ddf462d49295e73e6
Leon Oostrum <l.oostrum@esciencecenter.nl>
2023-05-16 14:33:51 +0200

    Revert "Remove use of shutil.which, not available on Mac runner"
    This reverts commit d86df30f53f25bc76a8c38eac386a943ff5ff7f6.

--- python/get_arduino_stm32_directory.py
+++ python/get_arduino_stm32_directory.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
 
 from pathlib import Path
+from shutil import which
 import subprocess
 
 import yaml
 
 
 if __name__ == '__main__':
-    try:
-        raw_config = subprocess.Popen(['arduino-cli', 'config', 'dump'], stdout=subprocess.PIPE).communicate()[0]
-    except FileNotFoundError as e:
-        raise ValueError('Failed to run arduino-cli, is it installed?') from e
+
+    arduino_cli = which('arduino-cli')
+    if arduino_cli is None:
+        raise ValueError("arduino-cli not found")
+
+    raw_config = subprocess.Popen([arduino_cli, 'config', 'dump'], stdout=subprocess.PIPE).communicate()[0]
     config = yaml.safe_load(raw_config)
     data_dir = Path(config['directories']['data'])
 
