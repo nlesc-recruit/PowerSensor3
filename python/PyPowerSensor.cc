bf602ff6d536aa8728f32f64536e593e642db4ff
Leon Oostrum <l.oostrum@esciencecenter.nl>
2023-07-27 11:58:02 +0200

    Add option to get sensor pair name to python interface

--- python/PyPowerSensor.cc
+++ python/PyPowerSensor.cc
@@ -23,7 +23,8 @@ PYBIND11_MODULE(powersensor, m) {
     .def("dump", &PowerSensor3::PowerSensor::dump,
       "Dump sensor values to file. Set filename to empty string to stop dumping", py::arg("filename"))
     .def("mark", static_cast<void (PowerSensor3::PowerSensor::*)(char)>(&PowerSensor3::PowerSensor::mark),
-      "Add given marker character to dump file", py::arg("Marker character"));
+      "Add given marker character to dump file", py::arg("Marker character"))
+    .def("get_name", &PowerSensor3::PowerSensor::getPairName, "Get sensor pair name", py::arg("pair_id"));
 
   py::class_<PowerSensor3::State>(m, "State")
     .def_readonly("consumed_energy", &PowerSensor3::State::consumedEnergy,
